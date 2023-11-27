18e82cde2145ad032f205005e3503813ea09a6b4
nick-pronin <vertupro061@gmail.com>
2023-11-27 14:59:49 +0300

    Fix homework

--- src/main/kotlin/lesson_01/task_2.kt
+++ src/main/kotlin/lesson_01/task_2.kt
@@ -4,15 +4,10 @@ fun main () {
     val numberOfOrders: Int = 75
     val thanksForOrderText: String = "Thank you for your order"
 
-    // Количество работников
-
     var numberOfEmployees: Int = 2000
 
     println(numberOfOrders)
     println(thanksForOrderText)
-    //  println(numberOfEmployees)
-
-    // Один работник уволился
 
     numberOfEmployees = 1999
 
