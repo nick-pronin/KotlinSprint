89e99fd27ea61733f5245b389e3f6dfee0f69ea4
nick-pronin <vertupro061@gmail.com>
2023-11-27 15:47:30 +0300

    Fix homework again

--- src/main/kotlin/lesson_01/task_5.kt
+++ src/main/kotlin/lesson_01/task_5.kt
@@ -1,11 +1,11 @@
 package lesson_01
 
-fun main () {
-    val totalSeconds = 6480
+val TOTAL_SECONDS = 6480
 
-    val hours = totalSeconds / 60 / 60
-    val minutes = (totalSeconds / 60) - (hours * 60)
-    val seconds = totalSeconds % 60
+fun main () {
+    val hours = TOTAL_SECONDS / 60 / 60
+    val minutes = (TOTAL_SECONDS / 60) - (hours * 60)
+    val seconds = TOTAL_SECONDS % 60
 
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
